810016eeb573bf3e06aa80aca6def4010fe95d7f
jaehyun <leejh3940@skuniv.ac.kr>
2025-04-11 09:32:30 +0900

    first commit

---
+++ src/main/java/com/jaehyun/JenkinscicdApplication.java
@@ -0,0 +1,13 @@
+package com.jaehyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinscicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinscicdApplication.class, args);
+	}
+
+}
