588064e35e41fb3c2e05aa7b3230c6bca8c9f7fe
jaehyun <leejh3940@skuniv.ac.kr>
2025-04-11 11:10:32 +0900

    second commit

--- src/main/java/com/jaehyun/controller/MainController.java
+++ src/main/java/com/jaehyun/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	@GetMapping
 	public String intro(Model model) {
-		model.addAttribute("greeting","Hello, Jenkins!");
+		model.addAttribute("greeting","Welcom CI/CD word!!!");
 		return "intro";
 	}
 }
